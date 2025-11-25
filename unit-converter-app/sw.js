c995d8ae5f9fa53b888ae9197e3d372f21bb28bd
TheFoxOz <fremaux.guillaume@gmail.com>
2025-11-25 18:01:05 +0000

    Add files via upload

---
+++ unit-converter-app/sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'convertbuddy-v1';
+const FILES = ['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png','/icons/apple-touch-icon.png'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
