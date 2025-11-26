3f4b3ba737004ed347c7063bd50403e418e5fdd9
TheFoxOz <fremaux.guillaume@gmail.com>
2025-11-26 17:36:46 +0000

    Update converter.js

--- unit-converter-app/public/scripts/converter.js
+++ unit-converter-app/public/scripts/converter.js
@@ -7,7 +7,7 @@ import { saveHistory } from "./firestore.js";
  * @param {object} el - DOM elements object
  * @param {string} category - Current conversion category
  * @param {function} onComplete - Callback to refresh UI history
- * @param {boolean} saveHistoryFlag - Whether to save this conversion to history
+ * @param {boolean} saveHistoryFlag - Whether to save this conversion to history (Default: true)
  */
 export function convertValue(el, category, onComplete, saveHistoryFlag = true) {
     const data = conversionData[category];
@@ -33,7 +33,7 @@ export function convertValue(el, category, onComplete, saveHistoryFlag = true) {
 
     el.toValue.value = (to.symbol ? to.symbol + " " : "") + formatted;
 
-    if (saveHistoryFlag) {
+    if (saveHistoryFlag) { // Check flag before saving
         saveHistory({
             category,
             fromUnit: el.fromUnit.value,
